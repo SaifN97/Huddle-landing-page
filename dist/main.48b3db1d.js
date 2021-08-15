99dad2460b083652ce4a84b0f5c6fd8ea544a2ba
SaifN97 <saifnarpali@gmail.com>
2021-08-15 22:22:45 +0530

    desktop version completed

--- dist/main.48b3db1d.js
+++ dist/main.48b3db1d.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\images\\bg-hero-mobile.svg":[["bg-hero-mobile.83217d71.svg","images/bg-hero-mobile.svg"],"images/bg-hero-mobile.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\images\\bg-hero-mobile.svg":[["bg-hero-mobile.83217d71.svg","images/bg-hero-mobile.svg"],"images/bg-hero-mobile.svg"],"./..\\images\\bg-hero-desktop.svg":[["bg-hero-desktop.9a31e454.svg","images/bg-hero-desktop.svg"],"images/bg-hero-desktop.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
